b8fc8952a05d06fdff6a34b8fc4953ca660002b2
Zeng Heng <zengheng810@gmail.com>
2016-09-29 14:41:29 +0800

    work queues

---
+++ src/worker.js
@@ -0,0 +1,18 @@
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var q = 'task_queue';
+    ch.assertQueue(q, {durable: true});
+    ch.prefetch(1);
+    console.log('[*] Waiting for message in %s.', q);
+    ch.consume(q, function(msg) {
+      var secs = msg.content.toString().split('.').length - 1;
+      console.log(" [x] Received %s", msg.content.toString());
+
+      setTimeout(function() {
+        console.log(" [x] Done");
+      }, secs * 1000);
+    }, {noAck: false});
+  });
+});
